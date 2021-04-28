7734dda6936cb80b949a7ac35d9838291aef061b
Yeshidema <12190106.gcit@rub.edu.bt>
2021-04-28 10:40:12 +0600

    Calculator

---
+++ Todo9/app/src/main/java/com/example/todo9/calculator.java
@@ -0,0 +1,16 @@
+package com.example.todo9;
+
+public class calculator {
+    public double add(double x,double y){
+        return x+y;
+    }
+    public double sub(double x,double y){
+        return x-y;
+    }
+    public double mul(double x,double y){
+        return x*y;
+    }
+    public double div(double x,double y){
+        return x/y;
+    }
+}
